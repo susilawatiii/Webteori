597244f39956841477355ea222cec22594fa5dfb
LAPTOP-V04BM7ME\- <susilawatiii256@gmail.com>
2023-05-07 22:50:34 +0800

    first commit

---
+++ controllers/MahasiswaController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace app\controllers;
+
+class MahasiswaController extends \yii\web\Controller
+{
+    public function actionProfil()
+    {
+        return $this->render('profil');
+    }
+
+}
